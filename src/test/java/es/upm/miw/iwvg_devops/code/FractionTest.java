8a1a28b9b01511890519ec65a4f4769f3e59e6c6
TrueAlexCv <alexcvtrue24282@gmail.com>
2024-09-23 20:41:58 +0200

    added tests for Fraction #6

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -31,4 +31,37 @@ public class FractionTest {
     void testDecimal() {
         assertEquals(0.2, fraction.decimal());
     }
+
+    @Test
+    void testIsProper() {
+        assertEquals(true, fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertEquals(false, fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        assertEquals(true, fraction.isEquivalent(new Fraction(2, 10)));
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(new Fraction(2, 5).toString(),
+                fraction.add(new Fraction(2, 10)).toString());
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(new Fraction(7, 15).toString(),
+                fraction.multiply(new Fraction(7, 3)).toString());
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(new Fraction(4, 15).toString(),
+                fraction.divide(new Fraction(6, 8)).toString());
+    }
 }
