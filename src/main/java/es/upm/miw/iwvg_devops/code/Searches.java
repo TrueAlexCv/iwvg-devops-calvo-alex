c971770e9d000c366595efea3e69fea525dd110f
TrueAlexCv <alexcvtrue24282@gmail.com>
2024-09-24 20:35:28 +0200

    added findUserIdByAllProperFraction and Test #7

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -0,0 +1,12 @@
+package es.upm.miw.iwvg_devops.code;
+
+import java.util.stream.Stream;
+
+public class Searches {
+
+    public Stream<String> findUserIdByAllProperFraction() {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream().allMatch(Fraction::isProper))
+                .map(User::getId);
+    }
+}
